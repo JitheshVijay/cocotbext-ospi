2ae146bfd7d2d8ecc8eb13475f07a30ae7dcf2c6
Jithesh Vijay <123816521+JitheshVijay@users.noreply.github.com>
2024-06-07 18:20:14 +0530

    Update ospi_flash.py

--- cocotbext/ospi/ospi_flash.py
+++ cocotbext/ospi/ospi_flash.py
@@ -1,56 +1,44 @@
-import sys
-import os
-
-# Add the path to the cocotbext directory
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '\cocotbext-ospi\cocotbext')))
-
-from cocotbext.ospi import OspiBus, OspiMaster, OspiSlave, OspiConfig
-
 import cocotb
-from cocotb.triggers import Timer, RisingEdge
-from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge, ReadOnly
+from cocotbext.ospi.ospi_bus import OspiBus
 
 class OspiFlash:
     def __init__(self, dut):
         self.dut = dut
         self.ospi_bus = OspiBus.from_prefix(dut, "OSPI")
-        self.ospi_config = OspiConfig(
-            word_width=8,
-            sclk_freq=50e6,
-            cpol=False,
-            cpha=False,
-            cs_active_low=True,
-            quad_mode=True  # Assuming quad_mode is a valid config for OSPI
-        )
-        self.ospi_master = OspiMaster(self.ospi_bus, self.ospi_config)
-
-    async def reset(self):
-        self.dut.OSPI_RST_b.value = 0
-        await Timer(100, units='ns')
-        self.dut.OSPI_RST_b.value = 1
-        await Timer(100, units='ns')
 
     async def initialize(self):
-        cocotb.start_soon(Clock(self.dut.clk, 10, units='ns').start())
-        await self.reset()
+        self.ospi_bus.cs.value = 1
+        await RisingEdge(self.ospi_bus.clk)
 
     async def write(self, address, data):
-        command = [0x02]  # Page Program command for OSPI
-        address_bytes = [(address >> i) & 0xFF for i in (16, 8, 0)]
-        data_bytes = [data]
-        await self.ospi_master.write(command + address_bytes + data_bytes)
-        await Timer(1, units='ns')
+        self.ospi_bus.cs.value = 0
+        await RisingEdge(self.ospi_bus.clk)
+        self.dut.address.value = address
+        self.dut.data_in.value = data
+        self.dut.write_enable.value = 1
+        await RisingEdge(self.ospi_bus.clk)
+        self.dut.write_enable.value = 0
+        self.ospi_bus.cs.value = 1
 
     async def read(self, address):
-        command = [0x03]  # Read Data command for OSPI
-        address_bytes = [(address >> i) & 0xFF for i in (16, 8, 0)]
-        await self.ospi_master.write(command + address_bytes)
-        await Timer(1, units='ns')
-        read_data = await self.ospi_master.read(1)
-        return read_data[0]
+        self.ospi_bus.cs.value = 0
+        await RisingEdge(self.ospi_bus.clk)
+        self.dut.address.value = address
+        self.dut.read_enable.value = 1
+        await RisingEdge(self.ospi_bus.clk)
+        await ReadOnly()
+        data = self.dut.data_out.value.integer
+        self.dut.read_enable.value = 0
+        self.ospi_bus.cs.value = 1
+        return data
 
     async def erase(self, address):
-        command = [0x20]  # Sector Erase command for OSPI
-        address_bytes = [(address >> i) & 0xFF for i in (16, 8, 0)]
-        await self.ospi_master.write(command + address_bytes)
-        await Timer(1, units='ns')
+        self.ospi_bus.cs.value = 0
+        await RisingEdge(self.ospi_bus.clk)
+        self.dut.address.value = address
+        self.dut.erase_enable.value = 1
+        await RisingEdge(self.ospi_bus.clk)
+        self.dut.erase_enable.value = 0
+        self.ospi_bus.cs.value = 1
+
