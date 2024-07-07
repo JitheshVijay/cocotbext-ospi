b56cc8cec9d96429f66b1b657e8c5130ee30f0a2
Jithesh Vijay <123816521+JitheshVijay@users.noreply.github.com>
2024-07-07 09:55:37 +0530

    Update ospi_flash.py

--- cocotbext/ospi/ospi_flash.py
+++ cocotbext/ospi/ospi_flash.py
@@ -1,44 +1,24 @@
 import cocotb
-from cocotb.triggers import RisingEdge, ReadOnly
 from cocotbext.ospi.ospi_bus import OspiBus
+from cocotb.triggers import RisingEdge, Timer
 
 class OspiFlash:
     def __init__(self, dut):
         self.dut = dut
-        self.ospi_bus = OspiBus.from_prefix(dut, "OSPI")
+        self.ospi_bus = OspiBus(dut, "OSPI_CLK", "OSPI_CS", "OSPI_IO", 8)
 
     async def initialize(self):
-        self.ospi_bus.cs.value = 1
-        await RisingEdge(self.ospi_bus.clk)
+        self.dut.reset_n <= 0
+        await Timer(10, units='ns')
+        self.dut.reset_n <= 1
 
-    async def write(self, address, data):
-        self.ospi_bus.cs.value = 0
-        await RisingEdge(self.ospi_bus.clk)
-        self.dut.address.value = address
-        self.dut.data_in.value = data
-        self.dut.write_enable.value = 1
-        await RisingEdge(self.ospi_bus.clk)
-        self.dut.write_enable.value = 0
-        self.ospi_bus.cs.value = 1
+    async def write(self, address, data, mode=0):
+        await self.ospi_bus.write(command=0x02, address=address, data=data, mode=mode)
 
-    async def read(self, address):
-        self.ospi_bus.cs.value = 0
-        await RisingEdge(self.ospi_bus.clk)
-        self.dut.address.value = address
-        self.dut.read_enable.value = 1
-        await RisingEdge(self.ospi_bus.clk)
-        await ReadOnly()
-        data = self.dut.data_out.value.integer
-        self.dut.read_enable.value = 0
-        self.ospi_bus.cs.value = 1
-        return data
+    async def read(self, address, mode=0):
+        return await self.ospi_bus.read(command=0x03, address=address, mode=mode)
 
-    async def erase(self, address):
-        self.ospi_bus.cs.value = 0
-        await RisingEdge(self.ospi_bus.clk)
-        self.dut.address.value = address
-        self.dut.erase_enable.value = 1
-        await RisingEdge(self.ospi_bus.clk)
-        self.dut.erase_enable.value = 0
-        self.ospi_bus.cs.value = 1
+    async def erase(self, address, mode=0):
+        await self.ospi_bus.write(command=0x20, address=address, data=0xFF, mode=mode)
+
 
