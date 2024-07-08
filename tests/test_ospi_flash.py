e2dd53ef25f24ee15bb682c0ca6015a35cab595f
Jithesh Vijay <123816521+JitheshVijay@users.noreply.github.com>
2024-07-08 11:10:35 +0530

    Update test_ospi_flash.py

--- tests/test_ospi_flash.py
+++ tests/test_ospi_flash.py
@@ -6,8 +6,7 @@ from cocotbext.ospi.ospi_flash import OspiFlash  # Adjust this import based on y
 async def test_ospi_flash_fast_read(dut):
     clk = "OSPI_CLK"
     cs = "OSPI_CS"
-    io = "OSPI_IO0", "OSPI_IO1", "OSPI_IO2", "OSPI_IO3",
-"OSPI_IO4", "OSPI_IO5", "OSPI_IO6", "OSPI_IO7"
+    io = "OSPI_IO"
     ospi = OspiFlash(dut, clk, cs, io)
     await ospi.initialize()
 
@@ -36,8 +35,7 @@ async def test_ospi_flash_fast_read(dut):
 async def test_ospi_flash_io_operations(dut):
     clk = "OSPI_CLK"
     cs = "OSPI_CS"
-    io = "OSPI_IO0", "OSPI_IO1", "OSPI_IO2", "OSPI_IO3",
-"OSPI_IO4", "OSPI_IO5", "OSPI_IO6", "OSPI_IO7"
+    io = "OSPI_IO"
     ospi = OspiFlash(dut, clk, cs, io)
     await ospi.initialize()
 
@@ -66,8 +64,7 @@ async def test_ospi_flash_io_operations(dut):
 async def test_ospi_flash_hold_operations(dut):
     clk = "OSPI_CLK"
     cs = "OSPI_CS"
-    io = "OSPI_IO0", "OSPI_IO1", "OSPI_IO2", "OSPI_IO3",
-"OSPI_IO4", "OSPI_IO5", "OSPI_IO6", "OSPI_IO7"
+    io = "OSPI_IO"
     ospi = OspiFlash(dut, clk, cs, io)
     await ospi.initialize()
 
