e7912815a415c293a2b733425f62b54ecfa3bdac
Jithesh Vijay <123816521+JitheshVijay@users.noreply.github.com>
2024-07-16 21:48:11 +0530

    Update ospi_flash.py

--- cocotbext/ospi/ospi_flash.py
+++ cocotbext/ospi/ospi_flash.py
@@ -18,22 +18,27 @@ class OspiFlash:
     async def write(self, address, data, mode=0):
         await self.ospi_bus.write(command=0x02, address=address, data=data, mode=mode)
 
+    async def read(self, address, length, mode=0):
+        return await self.ospi_bus.read(command=0x03, address=address, mode=mode, length=length)
+
     async def fast_read(self, address, length, mode=0):
         if not isinstance(address, (list, tuple)):
             address = [address]
         return await self.ospi_bus.read(command=0x0B, address=address, mode=mode, length=length)
 
-
-    async def fast_read(self, address, length, mode=0):
-        return await self.ospi_bus.read(command=0x0B, address=address, mode=mode, length=length)
-
     async def hold_operation(self):
-        self.dut.HOLD_N <= 0
-        await Timer(10, units='ns')
-        self.dut.HOLD_N <= 1
+        if hasattr(self.dut, 'HOLD_N'):
+            self.dut.HOLD_N.value = 0
+            await Timer(10, units='ns')
+            self.dut.HOLD_N.value = 1
+        else:
+            raise AttributeError("HOLD_N signal is not defined in the DUT")
 
     async def release_hold(self):
-        self.dut.HOLD_N <= 1
-        await Timer(10, units='ns')
+        if hasattr(self.dut, 'HOLD_N'):
+            self.dut.HOLD_N.value = 1
+            await Timer(10, units='ns')
+        else:
+            raise AttributeError("HOLD_N signal is not defined in the DUT")
 
 
