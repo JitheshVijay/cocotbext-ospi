959600560e2fdbbcc5fa7309e2d04241dcd77bab
Jithesh Vijay <123816521+JitheshVijay@users.noreply.github.com>
2024-09-12 13:04:09 +0530

    Update __init__.py

--- cocotbext/ospi/__init__.py
+++ cocotbext/ospi/__init__.py
@@ -1,7 +1,5 @@
 from .ospi_bus import OspiBus
-from .ospi_master import OspiMaster
-from .ospi_slave import OspiSlave
 from .ospi_config import OspiConfig
 from .ospi_flash import OspiFlash
 
-__all__ = ["OspiBus", "OspiMaster", "OspiSlave", "OspiConfig", "OspiFlash"]
+__all__ = ["OspiBus", "OspiConfig", "OspiFlash"]
